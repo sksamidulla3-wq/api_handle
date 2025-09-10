cf71a34b4395654bc548707ed56c03aba46b6767
samid <sksamidulla3@gmail.com>
2025-09-10 10:32:10 +0530

    commit

---
+++ lib/todo/model.dart
@@ -0,0 +1,42 @@
+class DataTodoModel {
+  int? limit;
+  int? skip;
+  int? total;
+  List<TodoModel>? todos;
+
+  DataTodoModel({
+    required this.limit,
+    required this.skip,
+    required this.total,
+    required this.todos,
+  });
+
+  factory DataTodoModel.fromJson(Map<String, dynamic> json) {
+    List<TodoModel> listTodo = [];
+    for (Map<String, dynamic> each in json["todo"]) {
+      listTodo.add(TodoModel.fromJson(each));
+    }
+    return DataTodoModel(
+      limit: json["limit"],
+      skip: json["skip"],
+      total: json["total"],
+      todos: listTodo,
+    );
+  }
+}
+
+class TodoModel {
+  String? completed;
+  String? todo;
+  int? id;
+
+  TodoModel({required this.completed, required this.todo, required this.id});
+
+  factory TodoModel.fromJson(Map<String, dynamic> json) {
+    return TodoModel(
+      completed: json["completed"],
+      todo: json["todo"],
+      id: json["id"],
+    );
+  }
+}
